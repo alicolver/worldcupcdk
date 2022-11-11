1cc8893ead266416179c59abd8edbb5e12fbed9f
Alisdair Colver <alicolver@Alisdairs-MacBook-Air.local>
2022-11-11 20:51:04 +0000

    Initialising CDK app

---
+++ test/worldcupcdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Worldcupcdk from '../lib/worldcupcdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/worldcupcdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Worldcupcdk.WorldcupcdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});

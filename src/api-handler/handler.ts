e7a28bd22cee427a07891b2c8cd88e31f547aab2
Simon Archer <simon.james.archer@gmail.com>
2022-11-12 13:18:38 +0000

    Does this work

--- src/api-handler/handler.ts
+++ src/api-handler/handler.ts
@@ -1,3 +1,26 @@
-export const handler = () => {
-  console.log("Hello, world!")
-}
+import {
+  APIGatewayProxyEvent,
+  Context,
+  APIGatewayProxyResult,
+} from "aws-lambda";
+import { loginHandler } from "./routes/auth/login";
+
+export const handler = async (
+  event: APIGatewayProxyEvent,
+  context: Context
+): Promise<APIGatewayProxyResult> => {
+  const endpoint = event.path;
+  switch (endpoint) {
+    case "/auth/login": {
+      return loginHandler();
+    }
+    default: {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          message: "Internal server error",
+        }),
+      };
+    }
+  }
+};

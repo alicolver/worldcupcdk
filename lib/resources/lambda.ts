083c4eecb3fdb2073c42a81cd1924180ebd014c7
Simon Archer <simon.james.archer@gmail.com>
2022-11-13 12:41:13 +0000

    Adding tables

--- lib/resources/lambda.ts
+++ lib/resources/lambda.ts
@@ -1,32 +1,45 @@
 import { Table } from "aws-cdk-lib/aws-dynamodb"
 import { Function as LambdaFunction, Runtime } from "aws-cdk-lib/aws-lambda"
 import { Construct } from "constructs"
-import { NodejsFunction, NodejsFunctionProps } from "aws-cdk-lib/aws-lambda-nodejs"
+import {
+  NodejsFunction,
+  NodejsFunctionProps,
+} from "aws-cdk-lib/aws-lambda-nodejs"
 import { join } from "path"
 import { UserPool, UserPoolClient } from "aws-cdk-lib/aws-cognito"
 import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam"
 
 interface LambdaProps {
-  dynamoTable: Table;
+  matchesTable: Table;
+  predictionsTable: Table;
+  pointsTable: Table;
+  leagueTable: Table;
+  usersTable: Table;
   cognitoUserClient: UserPoolClient;
   cognitoUserPool: UserPool;
 }
 
 export class Lambda {
-
-  readonly function : LambdaFunction
+  readonly function: LambdaFunction
 
   constructor(scope: Construct, props: LambdaProps) {
     const nodeJsFunctionProps: NodejsFunctionProps = {
       bundling: {
-        externalModules: [
-          "aws-sdk",
-        ],
+        externalModules: ["aws-sdk"],
       },
-      depsLockFilePath: join(__dirname, "../../../", "src", "package-lock.json"),
+      depsLockFilePath: join(
+        __dirname,
+        "../../../",
+        "src",
+        "package-lock.json"
+      ),
       environment: {
         PRIMARY_KEY: "itemId",
-        TABLE_NAME: props.dynamoTable.tableName,
+        MATCHES_TABLE_NAME: props.matchesTable.tableName,
+        PREDICTIONS_TABLE_NAME: props.predictionsTable.tableName,
+        POINTS_TABLE_NAME: props.pointsTable.tableName,
+        LEAGUE_TABLE_NAME: props.leagueTable.tableName,
+        USERS_TABLE_NAME: props.usersTable.tableName,
         USER_POOL_CLIENT_ID: props.cognitoUserClient.userPoolClientId,
         USER_POOL_ID: props.cognitoUserPool.userPoolId,
       },
@@ -38,7 +51,11 @@ export class Lambda {
       ...nodeJsFunctionProps,
     })
 
-    props.dynamoTable.grantReadWriteData(this.function)
+    props.matchesTable.grantReadWriteData(this.function)
+    props.predictionsTable.grantReadWriteData(this.function)
+    props.pointsTable.grantReadWriteData(this.function)
+    props.leagueTable.grantReadWriteData(this.function)
+    props.usersTable.grantReadWriteData(this.function)
     this.function.addToRolePolicy(
       new PolicyStatement({
         effect: Effect.ALLOW,

8ec04ddb23bace818c99323df4c6a4aadac38698
Luke Ely <le2417@ic.ac.uk>
2022-11-15 20:35:26 +0000

    Fix getUser

--- src/api-handler/routes/auth/utils.ts
+++ src/api-handler/routes/auth/utils.ts
@@ -1,7 +1,5 @@
 import { cognito } from "../../utils/clients"
-import { InitiateAuthCommand, GetUserCommand, GetUserCommandOutput } from "@aws-sdk/client-cognito-identity-provider"
-
-const USER_POOL_CLIENT_ID = process.env.USER_POOL_CLIENT_ID as string
+import { GetUserCommand, GetUserCommandOutput } from "@aws-sdk/client-cognito-identity-provider"
 
 interface userAuth {
   success: boolean
@@ -9,15 +7,8 @@ interface userAuth {
 }
 
 export const getUser = async (token: string): Promise<userAuth> => {
-  try {
-    await cognito.send(new InitiateAuthCommand({
-      AuthFlow: "USER_SRP_AUTH",
-      ClientId: USER_POOL_CLIENT_ID,
-      AuthParameters: {
-        AccessToken: token,
-      }
-    }))
 
+  try {
     const user = await cognito.send(new GetUserCommand({ AccessToken: String(token) }))
 
     return {
@@ -41,6 +32,6 @@ export const checkUserId = (userId: string | undefined): string => {
 
 export const getUserId = (user: GetUserCommandOutput): string => {
   return checkUserId(user.UserAttributes?.filter(
-    (attribute) => attribute.Name === "sub"
+    attribute => attribute.Name === "sub"
   )[0].Value)
 }

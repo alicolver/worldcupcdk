048abc98c772e883e304a3d3d2ed5a902465ffa4
Simon Archer <simon.james.archer@gmail.com>
2022-11-17 19:45:57 +0000

    Changes dates to matchdays for fetching matches

--- src/api-handler/routes/match/getUpcoming.ts
+++ src/api-handler/routes/match/getUpcoming.ts
@@ -1,20 +1,22 @@
 import { ScanCommand, ScanCommandInput } from "@aws-sdk/client-dynamodb"
 import { matchesTableSchema } from "../../../common/dbModels/models"
 import { unmarshall } from "@aws-sdk/util-dynamodb"
-import { addDays, getFormattedDate } from "../../utils/date"
+import { addDays, getFormattedDate, numberOfPreviousMatchDays } from "../../utils/date"
 import { MATCHES_TABLE_NAME } from "../../utils/database"
 import { DATABASE_ERROR, returnError } from "../../utils/constants"
 import express from "express"
 import { dynamoClient } from "../../utils/clients"
 
 export const getUpcomingMatchHandler: express.Handler = async (req, res) => {
+
   const today = getFormattedDate(new Date())
-  const tomorrow = getFormattedDate(addDays(new Date(), 1))
+  const mostRecentMatchDay = numberOfPreviousMatchDays[today] || 0
+  const nextMatchDay = mostRecentMatchDay + 1
   const params: ScanCommandInput = {
-    FilterExpression: "matchDate = :matchDate1 OR matchDate = :matchDate2",
+    FilterExpression: "matchDay = :matchDay1 OR matchDay = :matchDay2",
     ExpressionAttributeValues: {
-      ":matchDate1": { S: today },
-      ":matchDate2": { S: tomorrow }
+      ":matchDay1": { N: mostRecentMatchDay.toString() },
+      ":matchDay2": { N: nextMatchDay.toString() }
     },
     TableName: MATCHES_TABLE_NAME
   }
@@ -25,7 +27,10 @@ export const getUpcomingMatchHandler: express.Handler = async (req, res) => {
     const futureMatches = parsedMatches?.filter(match => match.isFinished === false)
 
     res.status(200)
-    res.json({ message: "Successfully fetched matches", data: futureMatches })
+    res.json({ message: "Successfully fetched matches", data: {
+      imminentMatches: futureMatches?.filter(match => match.matchDay === mostRecentMatchDay),
+      nextMatches: futureMatches?.filter(match => match.matchDay === nextMatchDay)
+    } })
   } catch (error) {
     console.log(error)
     return returnError(res, DATABASE_ERROR)
